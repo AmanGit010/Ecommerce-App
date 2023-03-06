91832ef9dcdea3a7de73555be2b2efcab581265a
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-03-06 20:00:40 +0530

    Changed App's launcher icon for both Android and iOS

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -6,8 +6,7 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final imageurl =
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    final Image = "Images/profile.png";
     return Drawer(
       child: Container(
         color: Colors.deepPurple,
@@ -22,7 +21,7 @@ class MyDrawer extends StatelessWidget {
                 accountName: Text("Amanjot Singh"),
                 accountEmail: Text("amanjot3290@gmail.com"),
                 currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(imageurl),
+                  backgroundImage: AssetImage(Image),
                 ),
               ),
             ),
